de9394655fe2b434c75f4ba1376ca65eb6f17f7d
Yurcik <yurashubni@bk.ru>
2016-11-28 19:24:49 +0200

    second commit : output messege edit

--- src/com/brain/academy/hello/HelloBrainAcademy.java
+++ src/com/brain/academy/hello/HelloBrainAcademy.java
@@ -4,6 +4,7 @@ public class HelloBrainAcademy {
 
     public static void main(String[] args) {
         System.out.println("Hello, Brain Academy!");
+        System.out.println("Second comment message");
     }
 }
 
